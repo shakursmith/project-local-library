9abd0b09822c248c226e9d6083d4b0f4bf5ca39d
Shakur Smith <57385924+shakursmith@users.noreply.github.com>
2020-12-27 22:37:30 -0600

    Add files via upload

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,10 +1,50 @@
-function findAccountById(accounts, id) {}
+function findAccountById(accounts, id) {
+  // Filter
+  const foundAccount = accounts.filter((account) => account.id === id);
+  return foundAccount[0];
+}
 
-function sortAccountsByLastName(accounts) {}
+const sortAccountsByLastName = accounts =>  {
+  let sortedByLast;
+  for (account in accounts) {
+    // Sort
+    sortedByLast = accounts.sort((account1, account2) => account1.name.last > account2.name.last ? 1 : -1);
+  }
+  return sortedByLast;
+}
 
-function numberOfBorrows(account, books) {}
+// Destructuring
+const numberOfBorrows = (accounts, books) => {
+  let borrowsCount = 0;
+  let tempArray; 
+    for (book in books) {
+      // Map
+      tempArray = books[book].borrows.map((borrowArray) => {
+        return borrowArray.id
+      });
+      // Filter
+      tempArray = tempArray.filter((id) => id === accounts.id);
+      if (tempArray.length > 0) borrowsCount++
+    }
+  return borrowsCount 
+}
 
-function getBooksPossessedByAccount(account, books, authors) {}
+const getBooksPossessedByAccount = ({ id }, books, authors) => {
+  let checkedOut = [];
+    for (let book in books) {
+      let bookObj = books[book];
+      // Find
+      if (bookObj.borrows.find((borrowObj) => (borrowObj.id === id && borrowObj.returned == false))) {
+        for (let author in authors) {
+          if (bookObj.authorId === authors[author].id) {
+            bookObj.author = authors[author];
+            checkedOut.push(bookObj);
+          }
+        }
+      }
+    }
+  return checkedOut;
+}
 
 module.exports = {
   findAccountById,
--- public/src/home.js
+++ public/src/home.js
@@ -1,14 +1,88 @@
-function totalBooksCount(books) {}
+const { findAuthorById } = require('./books');
 
-function totalAccountsCount(accounts) {}
+const totalBooksCount = books => {
+  let totalBooks = 0;
+  for (book in books) {
+    books[book] ? totalBooks+=1 : totalBooks+=0;
+  }
+  return totalBooks;
+}
 
-function booksBorrowedCount(books) {}
+const totalAccountsCount = accounts => {
+  let totalAccounts = 0;
+  for (account in accounts) {
+    accounts[account] ? totalAccounts+=1 : totalAccounts+=0;
+  }
+  return totalAccounts;
+}
 
-function getMostCommonGenres(books) {}
+const booksBorrowedCount = books => {
+  let totalBorrowed = 0;
+  for (book in books) {
+    books[book].borrows.some((items) => items.returned === false) ? totalBorrowed += 1 : totalBorrowed +=0;
+  }
+  return totalBorrowed; 
+}
 
-function getMostPopularBooks(books) {}
+const getMostCommonGenres = books => {
+  let unorderedList = [];
+  // create object with genre and count
+  for (book in books) {
+    let name = books[book].genre;
+    let count = 1;
+    let found = false;
 
-function getMostPopularAuthors(books, authors) {}
+    // update count for duplicate genres
+    for (items in unorderedList) {
+      if (books[book].genre === unorderedList[items].name) {
+        unorderedList[items].count += 1;
+        found = true;
+      } 
+    }
+    // Object Shorthand
+    if (!found) unorderedList.push({name, count}) 
+  } 
+  // sort list
+  const orderedList = unorderedList.sort((obj1, obj2) => obj1.count < obj2.count ? 1 : -1);
+  return orderedList.slice(0,5);
+}
+
+const getMostPopularBooks = books => {
+  let unorderedList = [];
+  // create object with title and count
+  for (book in books) {
+    let name = books[book].title;
+    let count = 0;
+    books[book].borrows.forEach(borrow => {
+      count = books[book].borrows.length;    
+    });
+  // Object Shorthand
+    unorderedList.push({name, count})
+  }
+  // sort list
+  const orderedList = unorderedList.sort((obj1, obj2) => obj1.count < obj2.count ? 1 : -1);
+  return orderedList.slice(0,5);
+}
+
+
+const getMostPopularAuthors = (books, authors) => {
+  // Reduce
+  const unorderedList = books.reduce((acc, book) => {
+    const bookId = book.authorId;
+    // Helper Function
+    const foundAuthor = findAuthorById(authors, bookId);
+    const firstName = foundAuthor.name.first;
+    const lastName = foundAuthor.name.last;
+    const name = `${firstName} ${lastName}`
+    const count = book.borrows.length;
+    // Object Shorthand
+    acc.push({name, count});
+    return acc;
+  }, []);
+  // Sort List
+  const orderedList = unorderedList.sort((obj1, obj2) => obj1.count < obj2.count ? 1 : -1);
+  return orderedList.slice(0,5);
+}
 
 module.exports = {
   totalBooksCount,
